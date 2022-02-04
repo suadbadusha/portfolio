3647ef826a3c4be346bdf3bd26324f5c59f76fd7
suadbadusha <67995271+suadbadusha@users.noreply.github.com>
2022-02-05 00:48:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+  };
+  
+  function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // fade in observed elements that are in view
+        entry.target.classList.replace('fadeOut', 'fadeIn');
+      } else {
+        // fade out observed elements that are not in view
+        entry.target.classList.replace('fadeIn', 'fadeOut');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+  const fadeElms = document.querySelectorAll('.row');
+  fadeElms.forEach(el => observer.observe(el));
+
+  $("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbxMyR1cVvr_Ak5-pJYwSFaUeXhf3iepz37bJQSRVYkLCvZLU0MiZKgDX42f6rRwMAY3GQ/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    });
+});
