12aff12e8345a39714cb10fedc60b7f0d8aa8e58
suadbadusha <67995271+suadbadusha@users.noreply.github.com>
2022-03-01 01:29:45 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
 
-  const observerOptions = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.7
-  };
-  
-  function observerCallback(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // fade in observed elements that are in view
-        entry.target.classList.replace('fadeOut', 'fadeIn');
-      } 
-      
-    });
-  }
-  
-  const observer = new IntersectionObserver(observerCallback, observerOptions);
-  
-  const fadeElms = document.querySelectorAll('.row');
-  fadeElms.forEach(el => observer.observe(el));
 
   $("#submit-form").submit((e)=>{
     e.preventDefault()
